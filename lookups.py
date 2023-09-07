4e869ea5ab392233cfe30722de2adf7284f57e9f
ZahraaDk <zahraadokmak@gmail.com>
2023-09-08 01:40:39 +0300

    Assign

--- lookups.py
+++ lookups.py
@@ -5,8 +5,27 @@ class Error(Enum):
     DB_Connecting_Error = "Error Connecting To Database!"
     DB_ReturnQuery_Error = "Error Returning Query"
     DB_Return_Df_Error = "Error returning this file type" 
+    Return_From_CSV_Error = "Error returning this CSV file"
+    Return_From_Excel_Error = "Error returning this Excel file"
+    Return_From_SQL_Error = "Error returning this SQL file"
     DB_Executing_Error = "Error executing the query"
     DB_Getting_SchemaInfo_Error = "Error getting the schema information"
+    Removing_Duplicates_Error = "Error removing duplicates"
+    Removing__Nulls_Error = "Error removing nulls"
+    Getting_Blanks_Error = "Error getting the rows with null values"
+    Getting_Shape_Error = "Error finding the shape of the dataframe"
+    Getting_Length_Error = "Error getting the length of the dataframe"
+    Not_Defined_Function_Error = "This function is not defined"
+    CSV_Error = "Error returning a list of csv files"
+
+
+class Pandas_Transformations(Enum):
+    Remove_Duplicates = "Remove Duplicates From Df"
+    Remove_Nulls = "Remove null values from Df"
+    Get_Blanks = "Get blanks from Df"
+    Get_Shape = "Find the shape of the Df"
+    Get_Length = "Get the total length of the Df"
+
 
 class Input_Types(Enum):
     CSV = 'CSV'
