3f8345d50ddf7c1203afd4f78a95690158cca892
ZahraaDk <zahraadokmak@gmail.com>
2023-09-12 17:04:42 +0300

    etl

--- prehook.py
+++ prehook.py
@@ -4,18 +4,29 @@ from lookups import Error, PreHookSteps, SQLTablesToReplicate, Input_Types, Sour
 from logging_handler import error_message
 import pandas as pd 
 
-def execute_sql_folder(db_session, sql_directory_path):
-    # [] for creating a new list, directory path: path to a directory where sql commands are stored.
-    sql_files = [sqlfile for sqlfile in os.listdir(sql_directory_path) if sqlfile.endswith('.sql')]
-    # for the execution to be ordered
+def execute_query_folder(db_session, query_directory_path):
+    query_files = os.listdir(query_directory_path)
+
+    sql_files = [file for file in query_files if file.endswith('.sql')]
+    csv_files = [file for file in query_files if file.endswith('.csv')]
     sorted_sql_files = sorted(sql_files)
+    sorted_csv_files = sorted(csv_files)
+
     for sql_file in sorted_sql_files:
-        # specifying the whole path to the file, and the file is opened for reading 
-        with open(os.path.join(sql_directory_path, sql_file), 'r') as file:
+        with open(os.path.join(query_directory_path, sql_file), 'r') as file:
             sql_query = file.read()
             return_val = execute_query(db_session=db_session, query=sql_query)
             if not return_val == Error.NO_ERROR:
-                raise Exception(f"{PreHookSteps.EXECUTE_SQL_QUERY.value} = SQL FILE Error on SQL File = " + str(sql_file))
+                raise Exception(f"{PreHookSteps.EXECUTE_SQL_QUERY.value} - SQL FILE Error on SQL File = " + str(sql_file))
+            
+    for csv_file in sorted_csv_files:
+        csv_path = os.path.join(query_directory_path, csv_file)
+        staging_df = pd.read_csv(csv_path, nrows=1) #Get the first row only to get column names
+        staging_df.columns = staging_df.columns.str.replace(' ', '_').str.replace('-', '_')
+        table_name = f"stg_{os.path.splitext(csv_file)[0]}"
+        table_name = table_name.replace(' ', '_').replace('-', '_')
+        create_statement = return_create_statement_from_df(staging_df, 'dw_reporting', table_name)
+        execute_query(db_session=db_session, query=create_statement)
 
 def return_tables_by_schema(schema_name):
     schame_tables = list()
@@ -38,26 +49,14 @@ def create_sql_staging_tables(db_session, source_name):
         create_statement = return_create_statement_from_df(staging_df, 'dw_reporting', dst_table)
         execute_query(db_session=db_session, query= create_statement)
 
-def execute_csv(db_session, csv_directory_path):
-    csv_files = [csv_file for csv_file in os.listdir(csv_directory_path) if csv_file.endswith('.csv')]
-    for csv_file in csv_files:
-        with open(os.path.join(csv_directory_path, csv_file), 'r') as csv_path:
-            csv_df = pd.read_csv(csv_path)
-            table = os.path.splitext(csv_file)[0]
-            staging_df = f"stg_{public_schema}_{table}"
-            create_stmt = return_create_statement_from_df(staging_df, 'dw_reporting', dst_table)
-            execute_query(db_session=db_session, query= create_stmt)
 
-def execute_prehook(sql_command_directory_path = './SQL_Commands'):
+def execute_prehook(query_directory_path = './Csv_Files'):
     try:
         db_session = create_connection()
         # Step 1:
-        execute_sql_folder(db_session, sql_command_directory_path) 
+        execute_query_folder(db_session, query_directory_path) 
         # Step 2 getting dvd rental staging:
         create_sql_staging_tables(db_session,SourceName.DVDRENTAL.value)
-        execute_csv(db_session, csv_directory)
-        # Step 3 getting college staging:
-        # create_sql_staging_tables(db_session,SourceName.COLLEGE)
         close_connection(db_session)
     except Exception as error:
         suffix = str(error)
